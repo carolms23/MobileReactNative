88a698e3a0729cd2f9a28e6f6a4f5b21db12a1c2
CAROLINE MARQUES DOS SANTOS <caroline.msantos26@senacsp.edu.br>
2023-11-05 13:21:41 -0300

    config

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Card from "./Card";
 
 export default function App() {
@@ -5,3 +6,25 @@ export default function App() {
 
   return <Card title="Beef Burguer" text="70 calorias" image="https://i.imgur.com/KinZFDx.jpg" price="12.00" />;
 }
+=======
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> 4f53db3 (Initial commit)
